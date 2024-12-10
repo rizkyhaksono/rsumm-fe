d654dd0d2f23ff7540cb3b17448911b8825e4a02
rizkyhaksono <mrizkyhaksono@gmail.com>
2024-12-10 20:23:47 +0700

    fix(theme): hydration on theme provider

--- src/components/layout/theme-provider.tsx
+++ src/components/layout/theme-provider.tsx
@@ -1,20 +1,18 @@
 "use client";
 
-import * as React from "react";
-import { ThemeProvider as NextThemesProvider } from "next-themes";
-type ThemeProviderProps = React.ComponentProps<typeof NextThemesProvider>;
-import { useEffect, useState } from "react";
-
-export function ThemeProvider({ children, ...props }: Readonly<ThemeProviderProps>) {
-  const [isLoaded, setIsLoaded] = useState(false);
-
-  useEffect(() => {
-    setIsLoaded(true);
-  }, []);
-
-  if (!isLoaded) {
-    return null;
+const NextThemesProvider = dynamic(
+  () => import("next-themes").then((e) => e.ThemeProvider),
+  {
+    ssr: false,
   }
+);
 
+type ThemeProviderProps = React.ComponentProps<typeof NextThemesProvider>;
+import dynamic from "next/dynamic";
+
+export function ThemeProvider({
+  children,
+  ...props
+}: Readonly<ThemeProviderProps>) {
   return <NextThemesProvider {...props}>{children}</NextThemesProvider>;
 }
